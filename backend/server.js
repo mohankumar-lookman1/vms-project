2e753528a8f5329398cb4e4b98b6296ef0dc7c45
mohankumar-lookman1 <dev.mohankumar.m@gmail.com>
2023-10-10 13:03:24 +0530

    server code add

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const http = require('http');
 const request = require('request');
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 
-app.get('/stream/index.m3u8', (req, res) => {
-    const sourceUrl = 'http://localhost:8083/stream/cam3/channel/0/hls/live/index.m3u8'; // Replace with your source HLS stream URL
+app.get('/0/stream/index.m3u8', (req, res) => {
+    const sourceUrl = 'http://localhost:8083/stream/cam1/channel/0/hls/live/index.m3u8'; // Replace with your source HLS stream URL
 
     request.get(sourceUrl)
         .on('error', (error) => {
@@ -18,6 +18,6 @@ app.get('/stream/index.m3u8', (req, res) => {
         .pipe(res);
 });
 
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
     console.log('Server running on port 3000');
 });
